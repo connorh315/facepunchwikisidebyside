be15e4b99d4bdf87e908dd03784a7623ec0aebca
Connor Harrison <connor3104@sky.com>
2020-06-12 23:28:39 +0100

    Initial commit

---
+++ sidebyside.js
@@ -0,0 +1,26 @@
+document.body.style.maxWidth = "none";
+var timer = null;
+
+var edit = document.getElementById("edit_value");
+var display = document.getElementById("edit_display");
+edit.style.maxWidth = display.style.maxWidth = "45%";
+
+var container = document.createElement("div");
+document.getElementsByTagName("form")[0].appendChild(container);
+container.style.float = "right";
+
+var previewButton = document.getElementById("previewbutton");
+previewButton.click();
+
+var preview = document.getElementById("preview");
+preview.style.maxWidth = "50%";
+preview.style.left = "unset";
+container.appendChild(preview)
+
+edit.addEventListener("input", (event) => {
+	clearTimeout(timer);
+	timer = setTimeout(function() {
+		preview.classList.remove("shown");
+		previewButton.click();
+	}, 3000);
+});
